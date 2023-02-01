6d3003a46045971872a5d5eadbb86106922172e6
Ahmed Maqbool <ahmedmaqx11@gmail.com>
2023-02-01 14:49:48 +0300

    inital commit

---
+++ consumer/consumer.js
@@ -0,0 +1,14 @@
+const amqp = require("amqplib");async function connect() {
+    try {
+      const connection = await amqp.connect("amqp://localhost:5672");
+      const channel = await connection.createChannel();
+      await channel.assertQueue("hello");
+      channel.consume("hello", message => {
+        console.log(message.content.toString())
+        channel.ack(message);
+      });
+      console.log(`Waiting for messages...`);
+    } catch (ex) {
+      console.error(ex);
+    }
+   }connect();
---
+++ producer/producer.js
@@ -0,0 +1,16 @@
+const amqp = require("amqplib");
+
+async function connect() {
+    const msgBuffer = Buffer.from("Hello World");
+    try {
+      const connection = await amqp.connect("amqp://localhost:5672");
+      const channel = await connection.createChannel();
+      await channel.assertQueue("hello");
+      await channel.sendToQueue("hello", msgBuffer);
+      console.log("Sending 'Hello World' to hello queue");
+      await channel.close();
+      await connection.close();
+    } catch (ex) {
+      console.error(ex);
+    }
+   }connect();
